f887a7d00d9ff4e7650d9989dfc5544b749c41c8
Brook Daba <brksolomon@gmail.com>
2022-04-05 12:43:46 -0400

    made lots of style updates simply using bootstrap updated env variables (secret key)

--- Core/Core/settings.py
+++ Core/Core/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e-qtbk)z4q)76fs-w)7czq+%f2ep9nw1zrk1vs*icdx(pk1lmj'
-
+SECRET_KEY=env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
